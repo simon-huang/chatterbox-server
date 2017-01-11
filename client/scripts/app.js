31d239da7c03a853058b59c945a753be5d4e0542
simon-huang <simon.t.huang@gmail.com>
2017-01-10 19:57:42 -0800

    Work on serving up static files

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -26,7 +26,7 @@ var app = {
     app.$roomSelect.on('change', app.handleRoomChange);
 
     // Fetch previous messages
-    app.startSpinner();
+    // app.startSpinner();
     app.fetch(false);
 
     // Poll for new messages
@@ -36,7 +36,7 @@ var app = {
   },
 
   send: function(message) {
-    app.startSpinner();
+    // app.startSpinner();
 
     // POST the message to the server
     $.ajax({
@@ -100,7 +100,7 @@ var app = {
   renderMessages: function(messages, animate) {
     // Clear existing messages`
     app.clearMessages();
-    app.stopSpinner();
+    // app.stopSpinner();
     if (Array.isArray(messages)) {
       // Add all fetched messages that are in our current room
       messages
@@ -206,7 +206,7 @@ var app = {
         app.$roomSelect.val(roomname);
       }
     } else {
-      app.startSpinner();
+      // app.startSpinner();
       // Store as undefined for empty names
       app.roomname = app.$roomSelect.val();
     }
@@ -225,15 +225,16 @@ var app = {
 
     // Stop the form from submitting
     event.preventDefault();
-  },
-
-  startSpinner: function() {
-    $('.spinner img').show();
-    //$('form input[type=submit]').attr('disabled', 'true');
-  },
-
-  stopSpinner: function() {
-    $('.spinner img').fadeOut('fast');
-    $('form input[type=submit]').attr('disabled', null);
   }
+  // ,
+
+  // startSpinner: function() {
+  //   $('.spinner img').show();
+  //   //$('form input[type=submit]').attr('disabled', 'true');
+  // },
+
+  // stopSpinner: function() {
+  //   $('.spinner img').fadeOut('fast');
+  //   $('form input[type=submit]').attr('disabled', null);
+  // }
 };
--- server/request-handler.js
+++ server/request-handler.js
@@ -19,28 +19,32 @@ var defaultCorsHeaders = {
 };
 
 var requestHandler = function(request, response) {
-  var messages;
   var fs = require('fs');
-
-  var statusCode;
   var headers = defaultCorsHeaders;
-  headers['Content-Type'] = 'text/plain';
+
+  // console.log(JSON.parse(htmlPageData));
+  var messages;
+  var statusCode;
+ 
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
   request.on('error', function(error) {
     statusCode = 404;
     response.writeHead(statusCode, headers);
+    response.write(htmlPageData);
     response.end('error', error);
   });
 
-  if (request.url.indexOf('/classes/messages') === -1) {
-    console.log('wrong endpoint');
-    statusCode = 404;
-    response.writeHead(statusCode, headers);
-    response.end('error');
- 
+  // if (request.url.indexOf('/classes/messages') === -1) {
+  //   console.log('wrong endpoint');
+  //   statusCode = 404;
+  //   response.writeHead(statusCode, headers);
+  //   response.write(htmlPageData);
+  //   response.end('error');
+
   
-  } else if (request.method === 'OPTIONS') {
+  // } else 
+  if (request.method === 'OPTIONS') {
     var headers = {};
     headers['Access-Control-Allow-Origin'] = '*';
     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS';
@@ -48,19 +52,61 @@ var requestHandler = function(request, response) {
     headers['Access-Control-Max-Age'] = '86400'; // 24 hours
     headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept';
     response.writeHead(200, headers);
+    response.write(htmlPageData);
     response.end();
   
   } else if (request.method === 'GET') {
-    fs.readFile('message.txt', (err, data) => {
-      if (err) {
-        console.log('error');
-        throw err;
-      }
-      messages = JSON.parse(data);
-      statusCode = 200;
-      response.writeHead(statusCode, headers);
-      response.end(JSON.stringify( {results: messages} ));
-    });  
+    //index.html 
+    if (request.url === '/' || request.url.indexOf('/username') !== -1) {
+      fs.readFile('./client/index.html', function(err, htmlPageData) {
+        headers['Content-Type'] = 'text/html';
+        response.writeHead(200, headers);  
+        response.write(htmlPageData);
+        response.end();
+      });
+    }
+      
+    if (request.url === '/styles/styles.css') {
+
+      fs.readFile('./client/styles/styles.css', function(err, cssfile) {
+        headers['Content-Type'] = 'text/css';
+        response.writeHead(200, headers);
+        response.write(cssfile);
+        response.end();
+      });
+
+    } else if (request.url === '/bower_components/jquery/dist/jquery.js') {
+
+      fs.readFile('./client/bower_components/jquery/dist/jquery.js', function(err, jquerryfile) {
+        headers['Content-Type'] = 'application/javascript';
+        response.writeHead(200, headers);
+        response.write(jquerryfile);
+        response.end();
+      });
+
+    } else if (request.url === '/scripts/app.js') {
+      fs.readFile('./client/scripts/app.js', function(err, app) {
+        headers['Content-Type'] = 'application/javascript';
+        response.writeHead(200, headers);
+        response.write(app);
+        response.end();
+      });
+    } else if (request.url.indexOf('/classes/messages') !== -1) {
+
+      fs.readFile('message.txt', (err, data) => {
+        if (err) {
+          console.log('error');
+          throw err;
+        }
+        messages = JSON.parse(data);
+        headers['Content-Type'] = 'application/json';
+        statusCode = 200;
+        response.writeHead(statusCode, headers);
+
+        response.end(JSON.stringify( {results: messages} ));
+      });
+    }
+
     
   } else if (request.method === 'POST') {
     fs.readFile('message.txt', (err, data) => {
@@ -71,6 +117,7 @@ var requestHandler = function(request, response) {
       messages = JSON.parse(data);
       statusCode = 201;
       response.writeHead(statusCode, headers);
+      response.write(htmlPageData);
       request.on('data', function(data) {
         var current = JSON.parse(data);
         current.objectId = messages.length + 1;
@@ -86,6 +133,8 @@ var requestHandler = function(request, response) {
     }); 
     
   }
+
+  
 };
 
 
